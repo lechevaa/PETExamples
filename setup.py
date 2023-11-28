ab0541ec9558c09b8548097700ab0808a417beed
rolfjl <rolo@norceresearch.no>
2023-11-28 09:13:55 +0100

    Setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Examples',
+    version='1.0',
+    packages=['3dBox', '3Spot', '3SpotEcalc', '3SpotRobust', '5SpotInverted', 'LinearModel', 'Rosenbrock'],
+    url='https://github.com/Python-Ensemble-Toolbox/Examples',
+    license_files=('LICENSE.txt',),
+    author_email='rolo@norceresearch.no',
+    description='Examples using the Python Ensemble Toolbox',
+)
